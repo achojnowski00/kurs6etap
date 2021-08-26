4a82021dc334f89c28d91976dafd096b1a02e074
Alex Chojnowski <achojnowski00@gmail.com>
2021-08-26 09:42:11 +0200

    add index.html, style css, and index.js with basic project of scroller in it

---
+++ sekcja9/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Hellow world!')
+
+  const rootElement = document.querySelector('#root');
+  const sections = document.querySelectorAll('section');
+  let currentSectionIndex = 0;
+  let isThrottled = false;
+
+  document.addEventListener('wheel', function (event) {
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false
+    }, 1000);
+
+    const direction = event.deltaY > 0 ? 1 : -1;
+    console.log(event.deltaY);
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+
+    currentSectionIndex += direction;
+    // console.log('current section: ' + currentSectionIndex)
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+
+  })
+})
