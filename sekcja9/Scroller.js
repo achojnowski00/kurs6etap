d7ec5ebb8e9a430cccde1bd364bfe62a490b24b0
Alex Chojnowski <achojnowski00@gmail.com>
2021-08-26 12:00:48 +0200

    Add navigation to scroller feature

--- sekcja9/Scroller.js
+++ sekcja9/Scroller.js
@@ -11,7 +11,8 @@ class Scroller {
     console.log(this.currentSectionIndex)
     this.isThrottled = false;
 
-    // this.isScrolledIntoView(this.sections[0]);
+    this.drawNavigation();
+    this.selectActiveNavItem();
   }
 
 
@@ -53,10 +54,44 @@ class Scroller {
   }
 
   scrollToCurrentSection = () => {
+    this.selectActiveNavItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: 'smooth',
       block: 'start',
     })
+  }
+
+  drawNavigation = () => {
+    this.navigationContainer = document.createElement('aside');
+    this.navigationContainer.setAttribute('class', 'scroller__navigation');
+    const list = document.createElement('ul');
+
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement('li');
+      listItem.addEventListener('click', () => {
+        this.currentSectionIndex = index;
+        this.scrollToCurrentSection();
+      })
+      list.appendChild(listItem);
+    })
+
+    this.navigationContainer.appendChild(list);
+
+    document.body.appendChild(this.navigationContainer);
+  }
+
+  selectActiveNavItem = () => {
+    if (this.navigationContainer) {
+      const navigationItems = this.navigationContainer.querySelectorAll('li');
+
+      navigationItems.forEach((item, index) => {
+        if (index === this.currentSectionIndex) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
+        }
+      })
+    }
   }
 
 }
